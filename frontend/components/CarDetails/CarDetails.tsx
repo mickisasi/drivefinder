1ce6cf8ad608e2e7b013d3c43c9726d36497f058
kcc18 <kcamarta2@nait.ca>
2023-11-19 10:45:34 -0700

    style updates on cardetails

--- frontend/components/CarDetails/CarDetails.tsx
+++ frontend/components/CarDetails/CarDetails.tsx
@@ -14,12 +14,17 @@ const CarDetails: React.FC<CarDetailsProps> = ({ make }) => {
         // Logic to fetch details based on the selected make
         // For simplicity, I'm using static content here
         if (selectedMake === 'Toyota' || selectedMake === 'toyota') {
-            setTitle('Toyota Corolla');
+            setTitle('Toyota');
             setDescription(
-                'The Toyota Corolla is one of the best-selling cars globally, and...'
+                'The benefits of purchasing a vehicle from Toyota are plentiful. The manufacturer offers a wide array of automobile options covering nearly every consumers needs and budget. The Toyota collection includes rugged off-roaders, small urban-focused hatchbacks, and everything in between. An often-overlooked bonus of purchasing a vehicle from one of the world’s largest producers is dependability.'
             );
         } else if (selectedMake === 'Dodge' || selectedMake === 'dodge') {
-            setTitle('Dodge Charger');
+            setTitle('Dodge');
+            setDescription(
+                'The Dodge Charger is a high-performance sedan known for its powerful engine and...'
+            );
+        } else if (selectedMake === 'Tesla' || selectedMake === 'tesla') {
+            setTitle('Tesla');
             setDescription(
                 'The Dodge Charger is a high-performance sedan known for its powerful engine and...'
             );
