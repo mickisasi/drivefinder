3c3d712fdb4c6de2c6a0bd2c35156039aee296e9
kcc18 <kcamarta2@nait.ca>
2023-11-19 06:41:15 -0700

    sidebar updates

--- frontend/components/Sidebar/sideBar.tsx
+++ frontend/components/Sidebar/sideBar.tsx
@@ -1,41 +1,77 @@
 import React from 'react';
-import './sideBar.module.css';
+import styles from './sideBar.module.css';
+import { useState } from 'react';
 
 export default function Sidebar() {
-  return (
-    <div className="sidebar">
-      <h2>Filter</h2>
 
-      <div className="filter">
-        <label htmlFor="make">Make:</label>
-        <select id="make" name="make">
-          <option value="all">All Makes</option>
-          <option value="toyota">Toyota</option>
-          <option value="tesla">Tesla</option>
-          {/* Add more options as needed */}
-        </select>
-      </div>
+    const [minPrice, setMinPrice] = useState('');
+    const [maxPrice, setMaxPrice] = useState('');
 
-      <div className="filter">
-        <label htmlFor="model">Model:</label>
-        <select id="model" name="model">
-          <option value="all">All Models</option>
-          <option value="corolla">Corolla</option>
-          <option value="model-s">Model S</option>
-          {/* Add more options as needed */}
-        </select>
-      </div>
+    const handleMinPriceChange = (event) => {
+        setMinPrice(event.target.value);
+    };
 
-      <div className="filter">
-        <label htmlFor="trim">Trim:</label>
-        <select id="trim" name="trim">
-          <option value="all">All trims</option>
-          <option value="SE">SE</option>
-          <option value="L">L</option>
-          {/* Add more options as needed */}
-        </select>
-      </div>
+    const handleMaxPriceChange = (event) => {
+        setMaxPrice(event.target.value);
+    };
 
-    </div>
-  );
+    return (
+        <div className="sidebar">
+            <h2>Filter</h2>
+
+            <div className="filter">
+                <label htmlFor="make">Make:</label>
+                <select id="make" name="make">
+                    <option value="all">All Makes</option>
+                    <option value="toyota">Toyota</option>
+                    <option value="tesla">Tesla</option>
+                    {/* Add more options as needed */}
+                </select>
+            </div>
+
+            <div className="filter">
+                <label htmlFor="model">Model:</label>
+                <select id="model" name="model">
+                    <option value="all">All Models</option>
+                    <option value="corolla">Corolla</option>
+                    <option value="model-s">Model S</option>
+                    {/* Add more options as needed */}
+                </select>
+            </div>
+
+            <div className="filter">
+                <label htmlFor="trim">Trim:</label>
+                <select id="trim" name="trim">
+                    <option value="all">All trims</option>
+                    <option value="SE">SE</option>
+                    <option value="L">L</option>
+                    {/* Add more options as needed */}
+                </select>
+            </div>
+
+            <h4>Price</h4>
+            <div className={styles.filterOption}>
+                <label htmlFor="minPrice">Min Price:</label>
+                <input
+                    type="number"
+                    id="minPrice"
+                    name="minPrice"
+                    value={minPrice}
+                    onChange={handleMinPriceChange}
+                />
+            </div>
+
+            <div className={styles.filterOption}>
+                <label htmlFor="maxPrice">Max Price:</label>
+                <input
+                    type="number"
+                    id="maxPrice"
+                    name="maxPrice"
+                    value={maxPrice}
+                    onChange={handleMaxPriceChange}
+                />
+            </div>
+
+        </div>
+    );
 }
