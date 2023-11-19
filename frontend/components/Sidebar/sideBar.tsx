b571dc4407fb817653b6cd4ecfd67d1a7691f4e7
kcc18 <kcamarta2@nait.ca>
2023-11-19 10:17:03 -0700

    updated styling

--- frontend/components/Sidebar/sideBar.tsx
+++ frontend/components/Sidebar/sideBar.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import styles from './sideBar.module.css';
 import { useState } from 'react';
 
-export default function Sidebar() {
 
+export default function Sidebar() {
     const [minPrice, setMinPrice] = useState('');
     const [maxPrice, setMaxPrice] = useState('');
 
@@ -16,12 +16,12 @@ export default function Sidebar() {
     };
 
     return (
-        <div className="sidebar">
+        <div className={styles.sidebar}>
             <h2>Filter</h2>
 
-            <div className="filter">
-                <label htmlFor="make">Make:</label>
-                <select id="make" name="make">
+            <div className={styles.filter}>
+                <label htmlFor="make" className={styles.label}>Make:</label>
+                <select id="make" name="make" className={styles.inputField}>
                     <option value="all">All Makes</option>
                     <option value="toyota">Toyota</option>
                     <option value="tesla">Tesla</option>
@@ -29,9 +29,9 @@ export default function Sidebar() {
                 </select>
             </div>
 
-            <div className="filter">
-                <label htmlFor="model">Model:</label>
-                <select id="model" name="model">
+            <div className={styles.filter}>
+                <label htmlFor="model" className={styles.label}>Model:</label>
+                <select id="model" name="model" className={styles.inputField}>
                     <option value="all">All Models</option>
                     <option value="corolla">Corolla</option>
                     <option value="model-s">Model S</option>
@@ -39,9 +39,9 @@ export default function Sidebar() {
                 </select>
             </div>
 
-            <div className="filter">
-                <label htmlFor="trim">Trim:</label>
-                <select id="trim" name="trim">
+            <div className={styles.filter}>
+                <label htmlFor="trim" className={styles.label}>Trim:</label>
+                <select id="trim" name="trim" className={styles.inputField}>
                     <option value="all">All trims</option>
                     <option value="SE">SE</option>
                     <option value="L">L</option>
@@ -52,27 +52,28 @@ export default function Sidebar() {
             <h4 className={styles.heading}>Price</h4>
 
             <div className={styles.filterOption}>
-                <label htmlFor="minPrice">Min Price:</label>
+                <label htmlFor="minPrice" className={styles.label}>Min Price:</label>
                 <input
                     type="number"
                     id="minPrice"
                     name="minPrice"
                     value={minPrice}
                     onChange={handleMinPriceChange}
+                    className={styles.inputField}
                 />
             </div>
 
             <div className={styles.filterOption}>
-                <label htmlFor="maxPrice">Max Price:</label>
+                <label htmlFor="maxPrice" className={styles.label}>Max Price:</label>
                 <input
                     type="number"
                     id="maxPrice"
                     name="maxPrice"
                     value={maxPrice}
                     onChange={handleMaxPriceChange}
+                    className={styles.inputField}
                 />
             </div>
-
         </div>
     );
 }
