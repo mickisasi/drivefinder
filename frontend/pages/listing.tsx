ceb6782a40a7879b00b1e2e07dee0738286eff8c
kcc18 <kcamarta2@nait.ca>
2023-11-18 22:01:13 -0700

    Added listing and featurescard components

---
+++ frontend/pages/listing.tsx
@@ -0,0 +1,10 @@
+import { FeaturesCard } from '@/components/FeaturesCard/FeaturesCard'
+import React from 'react'
+
+export default function listing() {
+  return (
+    <main>
+        <FeaturesCard />
+    </main>
+  )
+}
